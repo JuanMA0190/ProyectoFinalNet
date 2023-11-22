deea72fec9ba29529b4b2fe8fc6df5f494d3254d
Juan <juanma.a0190@gmail.com>
2023-11-22 18:40:02 -0300

    Trabajo terminado, falta comentar

---
+++ .vs/WindowsApplication1/Splash.vb
@@ -0,0 +1,28 @@
+﻿Public Class frm_Splash
+    Private Sub Timer_Muestra_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer_Muestra.Tick
+        Timer_EfectoCierr.Enabled = True
+        Timer_Muestra.Enabled = False
+    End Sub
+    Private Sub Timer_Efecto_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer_Efecto.Tick
+        If Me.Opacity < 1 Then
+            Me.Opacity = Me.Opacity + 0.1
+        Else
+            Timer_Efecto.Enabled = False
+            Timer_Muestra.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Timer_EfectoCierr_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer_EfectoCierr.Tick
+        If Me.Opacity > 0 Then
+            Me.Opacity = Me.Opacity - 0.1
+        Else
+            Timer_EfectoCierr.Enabled = False
+            frm_Login.Show()
+            Me.Hide()
+        End If
+    End Sub
+
+    Private Sub frm_Splash_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Opacity = 1
+    End Sub
+End Class
